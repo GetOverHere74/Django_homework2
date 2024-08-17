a5dba5d8d59a49452cf180f188cf9d2692da9590
skyl1 <skyl1.maxla2@mail.ru>
2024-08-17 16:36:38 +0500

    Контроллер для профиля

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import HiddenInput
 
 from catalog.forms import StyleFormMixin
 from users.models import User
@@ -8,3 +9,13 @@ class UserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ("email", "password1", "password2")
+
+
+class UserProfileForm(StyleFormMixin, UserChangeForm):
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "last_name", "phone", "avatar", "country"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password"].widget = HiddenInput()
